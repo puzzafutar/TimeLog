6b0dd13f25749dd114615435da0a1a55335729c3
czako <czakonorbert88@gmail.com>
2024-12-13 10:44:44 +0100

    Add Service Layer

---
+++ TimeLog.Service/Factory/TimeLogFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+using TimeLog.Domain;
+using TimeLog.Service.Factory.Interface;
+
+namespace TimeLog.Service.Factory
+{
+    public class TimeLogFactory : ITimeLogFactory
+    {
+        private readonly HttpClient _httpClient;
+
+        public TimeLogFactory(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<Domain.TimeLog> GetActiveInstanceAsync(int id)
+        {
+            return new Domain.TimeLog
+            {
+                TimerId = id,
+                Created = DateTime.UtcNow,
+                Description = await GetDescripitionAsync(),
+                Action = Domain.Action.Start,
+            };
+        }
+
+        public async Task<Domain.TimeLog> GetInActiveInstanceAsync(int id, string desc)
+        {
+            return new Domain.TimeLog
+            {
+                TimerId = id,
+                Created = DateTime.UtcNow,
+                Action = Domain.Action.Stop,
+                Description = desc,
+            };
+        }
+
+        public async Task<Domain.TimeLog> GetDeleteInstanceAsync(int id, string desc)
+        {
+            return new Domain.TimeLog
+            {
+                TimerId = id,
+                Created = DateTime.UtcNow,
+                Action = Domain.Action.Delete,
+                Description = desc,
+            };
+        }
+
+        public async Task<Domain.TimeLog> GetModifiedInstanceAsync(int id, string desc)
+        {
+            return new Domain.TimeLog
+            {
+                TimerId = id,
+                Created = DateTime.UtcNow,
+                Action = Domain.Action.Modify,
+                Description = desc,
+            };
+        }
+
+        private async Task<string> GetDescripitionAsync()
+        {
+            var response = await _httpClient.GetStringAsync("https://zenquotes.io/api/random");
+            var quote = JsonSerializer.Deserialize<List<Quote>>(response)?.FirstOrDefault();
+            return quote.q;
+        }
+    }
+}
---
+++ TimeLog.Service/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeLog.Service.Repository.Interface
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        IQueryable<T> AsQueryable();
+    }
+}
