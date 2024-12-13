11f95129d717cc2451da17f5ced4cf400b936e90
czako <czakonorbert88@gmail.com>
2024-12-13 10:09:13 +0100

    Add domains

---
+++ TimeLog.Domain/Quote.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeLog.Domain
+{
+    public class Quote
+    {
+        //Idézet
+        public string q { get; set; }
+
+        //Szerző
+        public string a { get; set; }
+    }
+}
